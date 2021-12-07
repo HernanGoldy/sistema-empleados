6d73d1a2703432cf559aec1ddd425bbcfd8a1252
HernanGoldy <hernanmdq@gmail.com>
2021-12-07 07:56:04 -0300

    Creamos rutas para cargar empleado

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 
 app= Flask(__name__)
@@ -14,14 +14,41 @@ mysql.init_app(app)
 
 @app.route('/')
 def index():
-    sql= "INSERT INTO `empleados`.`empleados` (`Nombre`, `Correo`, `Foto`) VALUES ('Juan', 'juan@gmail.com', 'fotojuan.jpg');"
     conn= mysql.connect()
     cursor= conn.cursor()
-    cursor.execute(sql)
+
+    sql= "SELECT * FROM empleados.empleados"
+    cursor.execute(sql) 
     
+    empleados= cursor.fetchall()
+
     conn.commit()
 
-    return render_template('empleados/index.html')
+    return render_template('empleados/index.html', empleados= empleados)
+
+
+@app.route('/create')
+def create():
+    return render_template('empleados/create.html')
+
+
+@app.route('/store', methods= ['post'])
+def store():
+    _nombre= request.form['txtNombre']
+    _correo= request.form['txtCorreo']
+    _foto= request.files['txtFoto']
+
+    sql= "INSERT INTO empleados.empleados (Nombre, Correo, Foto) VALUES (%s, %s, %s);"
+    datos= (_nombre, _correo, _foto.filename)
+    
+    conn= mysql.connect()
+    cursor= conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
+
+
 
 #NOTA - creamos un if para que muestre los errores que vayan ocurriendo mientras cargamos código
 if __name__ == '__main__':
