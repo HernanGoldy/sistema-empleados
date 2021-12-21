ba2795a4bf795157b2abe8a0af1d1fe3206daeb7
HernanGoldy <hernanmdq@gmail.com>
2021-12-21 08:31:39 -0300

    Agrego definniciones de la DB

---
+++ db-definition.sql
@@ -0,0 +1,16 @@
+create database if not exists sistema;
+use sistema;
+drop table if exists empleados;
+
+
+create table if not exists empleados (
+    id int not null auto_increment,
+    nombre varchar(255),
+    correo varchar(255),
+    foto varchar(5000),
+    primary key (id)
+);
+
+-- INDERT INTO empledos (nombre, correo, foto) VALUES ('Test', 'test@email.com', 'foto.jpg');
+
+-- SELECT * FROM empleados;
